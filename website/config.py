ee7f09bf91101b16c4ae3a38bf67c9a3b4b84761
Rajesh-dot <57223067+Rajesh-dot@users.noreply.github.com>
2021-09-07 15:10:35 +0530

    Update config.py

--- website/config.py
+++ website/config.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent
 
 class Config:
     SECRET_KEY = 'rajesh aka the king'
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{"database.db"}'
     UPLOAD_PATH = 'static'
-    IMAGE_UPLOADS = "D:\\Project\\Bynder\\website\\static\\img"
-    FILE_UPLOADS = "D:\\Project\\Bynder\\website\\static\\uploads"
+    IMAGE_UPLOADS = os.path.join(BASE_DIR, UPLOAD_PATH , "img") #"D:\\Project\\Bynder\\website\\static\\img"
+    FILE_UPLOADS = os.path.join(BASE_DIR, UPLOAD_PATH , "uploads") #"D:\\Project\\Bynder\\website\\static\\uploads"
